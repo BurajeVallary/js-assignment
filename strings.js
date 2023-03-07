c7c7279b3df2cabaedba17eea7d8274d5467215e
Vallary Buraje <vallaryburaje@gmail.com>
2023-03-07 12:51:24 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,55 @@
+///no.1
+let string ="beautiful";
+const words=item.slice(5);
+console.log(words);
+
+//no.2
+
+const food="I was feeling hungry today";
+let  us ="eat";
+var index = 5;
+var line =[food.slice(0,position)+ " " +us+ food.slice(position)]
+console.log(line);
+
+//no 3
+const story="The quick brown fox jumps over the lazy dog";
+let time =(story.match(/the/g) || []).length;
+console.log(time);
+ 
+let time1=(story.match(/brown/g) || []).length;
+console.log(time1);
+
+//no4
+const string1="we are going to school";
+const value ="now";
+console.log('The word "${value}" $ {string1.includes(value)? "is" : "is not"} in the sentence')
+const string2="The child was sittng on the table before it fell"
+const sit ="sitting"
+console.log('The word  "${word}   "{String2.includes{word}? "is" :"is not"}in the sentence ')
+
+//no5
+let one ="wonderful";
+let output =one.toUpperCase();
+console.log(output);
+
+let two ="amaizing";
+let see =two.toLowerCase;
+console.log(see);
+
+let three ="BEneath";
+let answer =three.toLowerCase;
+console.log(answer);
+
+const d ="A beautiful wedding";
+const e =d.split(" ");
+.map(w=>[0].toUpperCase () + w.substring(1).toLowerCase());
+.join (" ");
+console.log (e);
+
+) 
+
+
+
+
+
+
